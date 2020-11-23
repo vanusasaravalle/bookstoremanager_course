0b20623c8211e00176f9e4a4827d69d8ba7379e5
vanusasaravalle <vanusa.sousasaravalle@gmail.com>
2020-11-23 10:28:23 -0300

    First commit

---
+++ src/main/java/com/vansaravalle/bookstoremenager/BookstoremenagerApplication.java
@@ -0,0 +1,14 @@
+package com.vansaravalle.bookstoremenager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoremenagerApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(BookstoremenagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vansaravalle/bookstoremenager/BookstoremenagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vansaravalle.bookstoremenager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremenagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
