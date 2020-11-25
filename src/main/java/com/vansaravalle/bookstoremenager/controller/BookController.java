39ddda7927fbb8e136bbf7d304ff73a233c60884
vanusasaravalle <vanusa.sousasaravalle@gmail.com>
2020-11-25 10:57:17 -0300

    Criando o travis

--- src/main/java/com/vansaravalle/bookstoremenager/controller/BookController.java
+++ src/main/java/com/vansaravalle/bookstoremenager/controller/BookController.java
@@ -1,6 +1,9 @@
 package com.vansaravalle.bookstoremenager.controller;
 
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping ("/api/v1/books")
 public class BookController {
 
+
+    @ApiOperation(value = "Return an example hello world")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success method return")
+    })
     @GetMapping
     public String hello(){
         return "Olá mundo";
